fd0c0ab54264ae661a2961d9a865ca1ec5bad7d3
Enrico Benini <benkio89@gmail.com>
2023-06-08 23:47:01 +0100

    fourmolu instead of ormolu & hlint older version

--- src/Controls.hs
+++ src/Controls.hs
@@ -3,19 +3,19 @@ module Controls (Controls (..), defaultControls) where
 import SDL.Input.Keyboard.Codes
 
 data Controls = Controls
-  { up :: Keycode,
-    down :: Keycode,
-    left :: Keycode,
-    right :: Keycode,
-    quit :: Keycode
-  }
+    { up :: Keycode
+    , down :: Keycode
+    , left :: Keycode
+    , right :: Keycode
+    , quit :: Keycode
+    }
 
 defaultControls :: Controls
 defaultControls =
-  Controls
-    { up = KeycodeUp,
-      down = KeycodeDown,
-      left = KeycodeLeft,
-      right = KeycodeRight,
-      quit = KeycodeQ
-    }
+    Controls
+        { up = KeycodeUp
+        , down = KeycodeDown
+        , left = KeycodeLeft
+        , right = KeycodeRight
+        , quit = KeycodeQ
+        }
--- src/Graphics/Window.hs
+++ src/Graphics/Window.hs
@@ -11,19 +11,19 @@ import SDL.Video.Renderer (defaultRenderer)
 
 windowSize :: IO (CInt, CInt)
 windowSize = do
-  displays <- getDisplays
-  display <- maybe (fail "Display Not Found") pure $ listToMaybe displays
-  let (V2 x y) = displayBoundsSize display
-  return (x, y)
+    displays <- getDisplays
+    display <- maybe (fail "Display Not Found") pure $ listToMaybe displays
+    let (V2 x y) = displayBoundsSize display
+    return (x, y)
 
 initializeWindow :: IO (Window, Renderer)
 initializeWindow = do
-  (windowWidth, windowHeight) <- windowSize
-  window <- createWindow "TheBindingOfRichard" defaultWindow {windowInitialSize = V2 windowWidth windowHeight}
-  renderer <- createRenderer window (-1) defaultRenderer
-  return (window, renderer)
+    (windowWidth, windowHeight) <- windowSize
+    window <- createWindow "TheBindingOfRichard" defaultWindow{windowInitialSize = V2 windowWidth windowHeight}
+    renderer <- createRenderer window (-1) defaultRenderer
+    return (window, renderer)
 
 windowToBlack :: Renderer -> IO ()
 windowToBlack renderer = do
-  rendererDrawColor renderer $= V4 0 0 0 255
-  clear renderer
+    rendererDrawColor renderer $= V4 0 0 0 255
+    clear renderer
