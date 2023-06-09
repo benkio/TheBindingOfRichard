2a0c3e4aefd0fa94fc9d40dbb2c6039e8248a822
Enrico Benini <benkio89@gmail.com>
2023-06-09 20:50:49 +0100

    Add an initial HUnit test

---
+++ test/Main.hs
@@ -0,0 +1,27 @@
+module Main (main) where
+
+import Test.HUnit
+
+-- Function to test
+addTwoNumbers :: Int -> Int -> Int
+addTwoNumbers x y = x + y
+
+-- Test case
+testAddTwoNumbers :: Test
+testAddTwoNumbers =
+  TestCase $ do
+    let result = addTwoNumbers 2 3
+    assertEqual "Addition failed" 5 result
+
+-- Test suite
+testSuite :: Test
+testSuite =
+  TestList
+    [ TestLabel "Test addition" testAddTwoNumbers
+    ]
+
+-- Run the tests
+main :: IO ()
+main = do
+  counts <- runTestTT testSuite
+  putStrLn $ show counts
